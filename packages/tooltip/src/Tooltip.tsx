aa171999e8991f3183e1cecc7b7ce1a738446c60
Fel Fork <iljapuskov@gmail.com>
2026-03-12 20:26:58 +0100

    refactor(tooltip-provider): export

--- packages/tooltip/src/Tooltip.tsx
+++ packages/tooltip/src/Tooltip.tsx
@@ -2,10 +2,12 @@ import {TooltipRoot} from "./primitives/_tooltipRoot";
 import {TooltipTrigger} from "./primitives/_tooltipTrigger";
 import {TooltipPortal} from "./primitives/_tooltipPortal";
 import {TooltipContent} from "./primitives/_tooltipContent";
+import {TooltipProvider} from "./TooltipProvider";
 
 export const Tooltip = {
     Root: TooltipRoot,
     Trigger: TooltipTrigger,
     Portal: TooltipPortal,
     Content: TooltipContent,
+    Provider: TooltipProvider
 }
--- packages/tooltip/src/index.ts
+++ packages/tooltip/src/index.ts
@@ -4,3 +4,5 @@ export {TooltipContent} from "./primitives/_tooltipContent"
 export {TooltipRoot} from "./primitives/_tooltipRoot"
 export {TooltipPortal} from "./primitives/_tooltipPortal"
 export {TooltipTrigger} from "./primitives/_tooltipTrigger"
+
+export {TooltipProvider} from "./TooltipProvider"
